f6224d32af53e5d3c15b8ad094c3f1bdd29f7430
Gabe Westra <westratechnology@gmail.com>
2022-10-03 05:08:28 -0700

    Add Print Statements to Welcome Visitors

--- OperationFurry.py
+++ OperationFurry.py
@@ -6,3 +6,7 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is Loading
 """
+
+print("\n\nWelcome to Operation Furry InfoTech Center")
+
+print("\nOperation Furry's Operation System Is Booting Up")

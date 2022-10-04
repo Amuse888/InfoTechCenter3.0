38957efd15feec7d992f9424c5e72efcdc415d42
Gabe Westra <westratechnology@gmail.com>
2022-10-04 05:35:36 -0700

    Added color text to program

--- OperationFurry.py
+++ OperationFurry.py
@@ -10,7 +10,6 @@ drivers know that the InfoTechCenter OS is Loading
 #Import Libraries Here
 from time import sleep #We imported the Sleep function from the Time library
 
-
-print("\n\nWelcome to Operation Furry InfoTech Center")
+print("\033[1;36m  \n\nWelcome to Operation Furry InfoTech Center \n")
 sleep(2)
 print("\nOperation Furry's Operation System Is Booting Up")

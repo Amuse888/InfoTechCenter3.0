e92a22fef79992d500672923541c64eb5bd800da
Gabe Westra <westratechnology@gmail.com>
2022-10-05 05:38:52 -0700

    Initial Weather Commit

---
+++ OperationFurry.py
@@ -0,0 +1,10 @@
+#Weather
+#Developer: Gabriel Westra
+#Version 1.0
+
+"""
+Create a function for our current weather using the
+random.choice function to determine what the weather is
+picking from a list - using an If, Elif, and Else statements
+to check the condition and print a specific print line
+"""
